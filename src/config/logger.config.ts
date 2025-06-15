f50ef01622fed58f9f9f475c715da03eda926af7
mayurkanade44 <mayurkanade44@gmail.com>
2025-06-15 20:31:40 +0530

    logger error fixed

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,21 +1,21 @@
 import winston from "winston";
 
-
 const logger = winston.createLogger({
-    format: winston.format.combine(
-        winston.format.timestamp({ format: "MM-DD-YYYY HH:mm:ss"  }), // how the timestamp should be formatted
-        winston.format.json(), // Format the log message as JSON
-        // define a cutom print
-        winston.format.printf( ({  level, message, timestamp, ...data }) => {
-            const output = { 
-                level,
-                message, 
-                timestamp, 
-                data 
-            };
-            return JSON.stringify(output);
-        })
-    ),
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "MM-DD-YYYY HH:mm:ss" }), // how the timestamp should be formatted
+    winston.format.json(), // Format the log message as JSON
+    // define a cutom print
+    winston.format.printf(({ level, message, timestamp, ...data }) => {
+      const output = {
+        level,
+        message,
+        timestamp,
+        data,
+      };
+      return JSON.stringify(output);
+    })
+  ),
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
